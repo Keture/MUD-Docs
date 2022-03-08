4d827ca12f7c4954f5d63d977f38a31f52c007d8
unknown <josiah.prathaban@gmail.com>
2022-03-08 13:53:17 +0530

    home page playground

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,20 +1,39 @@
-import React, { useState } from 'react';
-import styles from './styles.module.css';
-
+import React, { useState } from "react";
+import CodeEditor from "@uiw/react-textarea-code-editor";
+import styles from "./styles.module.css";
+import {mu} from "./mark-up-down-1.0.0.min.js"
 
 export default function HomepageFeatures() {
   const [text, setText] = useState("");
 
-  const preview = (e) => {
-    setText(e.target.value);
-  }
+  const preview = (evn) => {
+    setText(evn.target.value);
+    setCode(evn.target.value);
+  };
+
+  const [code, setCode] = React.useState(
+    `# Headding\n## Sub Headding\n`
+  );
 
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
-          <textarea onKeyUp={preview}></textarea>
-          <textarea value={text}></textarea>
+        <div className={styles.container}>
+          <CodeEditor
+            value={code}
+            language="markdown"
+            placeholder="Please enter JS code."
+            onChange={(evn) => preview(evn)}
+            padding={12}
+            style={{
+              fontSize: 12,
+              backgroundColor: "#f5f5f5",
+              fontFamily:
+                "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+            }}
+          />
+          <div dangerouslySetInnerHTML={{ __html: mu(code, "\n","\n","==") }} />
+          {/* <div>{text}</div> */}
         </div>
       </div>
     </section>
